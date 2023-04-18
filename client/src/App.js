b93125e44de3b08ac9e055f901bea826e7ae0b4c
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-04-18 17:36:59 -0400

    Create basic user

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
+import {useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState();
+  
+  const loginFormSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:8000/users', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      mode: 'cors',
+      body: JSON.stringify({firstName})
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={loginFormSubmit}>
+          <input type="text" name="first_name" onChange={(e) => setFirstName(e.target.value)}/>
+          <button>Log In</button>
+        </form>
       </header>
     </div>
   );
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// POST request to create a new user
+router.post('/users', (req, res) => {
+
+  // Create new user with form information
+  const newUser = new User({
+    first_name: req.body.firstName,
+    tasks: []
+  })
+  newUser.save()
+
+  // Successfully saved new user
+  .then(new_user => {
+    return res.status(200).json({success: true, new_user: new_user})
+  })
+  // Unsuccessfully saved new user
+  .catch(err => {
+    return res.status(500).json({err: err, success: false})
+  })
+})
 
 module.exports = router;
