3a2d08f37ac39d408735dd53bb8e9356188393ab
Yvettecodinghard <y.yvette98k@gmail.com>
2023-04-19 15:41:52 -0700

    modify tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Task = require('../models/task');
 
 // Route to get all tasks
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     Task.find({}).then(tasks=>{res.status(200).json(tasks)}).catch(error=>{res.status(500).json(Error)});
     
 });
