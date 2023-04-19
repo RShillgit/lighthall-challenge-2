259cd65fa504b29ccc2f3655014dacd5d051dc65
Yvettecodinghard <y.yvette98k@gmail.com>
2023-04-19 15:30:10 -0700

    modify tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,14 +3,9 @@ const router = express.Router();
 const Task = require('../models/task');
 
 // Route to get all tasks
-router.get('/', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json(tasks);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error fetching tasks", error: error });
-  }
+router.get('/', (req, res) => {
+    Task.find({}).then(tasks=>{res.status(200).json(tasks)}).catch(error=>{res.status(500).json(Error)});
+    
 });
 
 module.exports = router;
