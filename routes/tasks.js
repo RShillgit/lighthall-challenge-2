3596f7f982c409f41bd8c3e6907242686c5e379c
Yvettecodinghard <y.yvette98k@gmail.com>
2023-04-19 15:44:14 -0700

    add{}

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,7 @@ const Task = require('../models/task');
 
 // Route to get all tasks
 router.post('/', (req, res) => {
-    Task.find({}).then(tasks=>{res.status(200).json(tasks)}).catch(error=>{res.status(500).json(Error)});
+    Task.find({}).then(tasks=>{res.status(200).json({tasks})}).catch(error=>{res.status(500).json({Error})});
     
 });
 
