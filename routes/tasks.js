2bbf7fa044514c534952c722e1ff7104e2e0e541
Yvettecodinghard <y.yvette98k@gmail.com>
2023-04-19 15:57:29 -0700

    move the index.js functions to tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,10 +2,52 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/task');
 
-// Route to get all tasks
-router.post('/', (req, res) => {
-    Task.find({}).then(tasks=>{res.status(200).json({tasks})}).catch(error=>{res.status(500).json({Error})});
-    
+// POST create a new task for a user.
+router.post('/', function(req, res, next) {
+  // Find user by id
+  User.findById(req.body.currentUserId)
+    .then(currentUser => {
+      // Create new task for user
+      const newTask = new Task({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status,
+        due_date: req.body.due_date,
+        user: currentUser._id
+      });
+      // Save task to database
+      newTask.save()
+        .then(savedTask => {
+          // Add task to user's tasks array
+          currentUser.tasks.push(savedTask._id);
+          currentUser.save()
+            .then(() => {
+              return res.status(200).json({success: true, message: "Task created successfully."});
+            })
+            .catch(err => {
+              return res.status(500).json({err: err, success: false});
+            });
+        })
+        .catch(err => {
+          return res.status(500).json({err: err, success: false});
+        });
+    })
+    .catch(err => {
+      return res.status(500).json({err: err, success: false});
+    });
+});
+
+// GET all tasks for a user.
+router.get('/', function(req, res, next) {
+  // Find user by id and populate their tasks
+  User.findById(req.query.currentUserId)
+    .populate('tasks')
+    .exec(function (err, user) {
+      if (err) {
+        return res.status(500).json({err: err, success: false});
+      }
+      return res.status(200).json({success: true, tasks: user.tasks});
+    });
 });
 
 module.exports = router;
